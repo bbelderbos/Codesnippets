5362ab96e35bcb7d4ffcac099e0531b50e446741
Bob Belderbos <bobbelderbos@gmail.com>
2012-12-23 17:07:43 +0100

    enhanced spanish tv guide (movies) crawler in python

--- python/cine_tv.py
+++ python/cine_tv.py
@@ -1,32 +1,90 @@
 #!/usr/bin/env python
-# -*- coding: latin-1 -*-
-
-import urllib
+# -*- coding: utf-8 -*-
+# Author: Bob Belderbos / written: Dec 2012
+# Purpose: get movies aired on Spanish tv to use in 24-hour cronjob
+#
+import pprint ;  pp = pprint.PrettyPrinter(indent=4)
+import urllib, sys
+from time import time
 from bs4 import BeautifulSoup as Soup
 
-# http://stackoverflow.com/questions/6116978/python-replace-multiple-strings
-def replace_all(text, dic):
-    for i, j in dic.iteritems():
-        text = text.replace(i, j)
-    return text
+class TvCine(object):
 
-reps = {'images/logo_':'', '.jpg':''}
+  def __init__(self):
+    """ Setup """
+    # show movies from 20-24 for now, todo: make these limits cli args
+    self.START_TIME = 20
+    self.END_TIME = 00
+    self.moviePage = "http://www.sincroguia.tv/todas-las-peliculas.html" 
+    self.movies = self.parse_movies()
+    #pp.pprint(self.movies); sys.exit()
 
-url = "http://parrilla-tv.lavanguardia.com/"
-soup = Soup(urllib.urlopen(url))
-movies = soup.find_all("li", { "class" : "cine inactiu" })
 
-for movie in movies:  
-	try:
-		channel = replace_all(movie.parent.parent.parent.img.attrs['src'], reps)
-		hour = movie.find_all("span", { "class" : "hora" })[0].get_text()
-		title = " ".join(movie.find_all("span", { "class" : "mes" })[0].get_text().split(': ')[1:]).encode('latin-1')
-		
-		print channel + "<br>"
-		print hour + "<br>" 
-		print title + "<br>"
-		print "<a href='http://imdbapi.com/?t=" + urllib.quote(title) +"' target='_blank'>http://imdbapi.com/?t=" + urllib.quote(title) + "</a><br>"
-		print "==<br><br>"
+  def parse_movies(self):
+    """ Import the movie URL """
+    soup = Soup(urllib.urlopen(self.moviePage))
+    movies = []
+    for link in soup.find_all("a"):
+      time = link.previous_sibling
+      url = link.get('href')
+      title = link.get('title')
+      if not "/peliculas/" in url: continue
+      if int(time[:2]) < self.START_TIME: continue
+      if time[:2] == self.END_TIME: break
+      (longTitle, verboseInfo) = self.get_movie_verbose_info(title, url)
+      movies.append({ 'time': time[0:6], 
+                      'title':longTitle.encode(encoding='UTF-8',errors='strict'), 
+                      'url': url.encode(encoding='UTF-8',errors='strict'), 
+                      'info': verboseInfo.encode(encoding='UTF-8',errors='strict'),  
+                    })
+    return movies
 
-	except: 
-		pass
+   
+  def get_movie_verbose_info(self, title, url):
+    html = self.read_url(url)
+    soup = self.filter_relevant_bits(html)
+    titleInfo = ficha = contentficha = ""; lineNum = 0
+    for line in soup.li.stripped_strings: 
+      ficha += line + "\n"
+    for line in soup.find_all('li')[1].stripped_strings:
+      lineNum += 1
+      if lineNum<3: titleInfo += line
+      contentficha += line + "\n"
+    # somtimes there is not a translated title, in that case echo the original title
+    titleInfo = titleInfo.replace("(", " (") if "(" in titleInfo else title
+    return (titleInfo, ficha+"\n"+contentficha)
+
+
+  def read_url(self, url):
+    f = urllib.urlopen(url) 
+    html = f.read()
+    f.close
+    return html
+
+  
+  def filter_relevant_bits(self, html):
+    a = html.split('class="ficha">')
+    movieInfo = a[1].split('<a href="javascript:;" onclick="remote')
+    soup = Soup(movieInfo[0]) 
+    return soup
+
+
+  def print_movie_titles(self): 
+    for m in self.movies:
+      print m['time'], m['title']
+      
+
+  def print_movie_details(self):
+    for m in self.movies:
+      print "+" * 110
+      print m['time'], m['title']
+      print "+" * 110
+      print "URL: \n" + m['url']
+      print "\nDetails: \n" + m['info']
+      print "\n\n"
+
+
+### instant
+t = TvCine()
+t.print_movie_titles()
+t.print_movie_details()
