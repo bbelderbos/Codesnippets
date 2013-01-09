ae9b27dfbf8ac5ad22507fcdcdb747232f8c8f3a
Bob Belderbos <bobbelderbos@gmail.com>
2013-01-09 09:02:14 +0100

    put featured image in own repo

--- css/featured_image/googlefonts.php
+++
@@ -1,6 +0,0 @@
-<?php 
-# thanks to http://phat-reaction.com/googlefonts.php
-$fontsSeraliazed = file_get_contents('http://phat-reaction.com/googlefonts.php?format=php');
-$fontArray = unserialize($fontsSeraliazed);
-#print_r($fontArray);
-?>
