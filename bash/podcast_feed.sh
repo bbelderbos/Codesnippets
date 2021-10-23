15444bc450f1d07a935988cde43a3f74163065c1
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-23 22:46:31 +0800

    Delete bash directory

--- bash/podcast_feed.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget $(wget -q -O - $1 | sed -n 's/.*enclosure.*url="\([^"]*\)" .*/\1/p')
--- bash/podcast_feed_print.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget -q -O - $1 | sed -n 's/.*enclosure.*url="\([^"]*\)" .*/# \1/p'
